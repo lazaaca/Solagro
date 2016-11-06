914fed8f67f809b428bbcc6333a0cbb86feefff7
peskic93 <peshkic@gmail.com>
2016-11-06 22:05:40 +0100

    Added maps to App

--- app/src/main/java/solagro/org/solagro20/MainActivity.java
+++ app/src/main/java/solagro/org/solagro20/MainActivity.java
@@ -1,11 +1,12 @@
 package solagro.org.solagro20;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openMapActivity(View view){
-        Intent intent = new Intent(this, MapActivity.class);
+        Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
 
     }
