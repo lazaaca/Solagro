74a661cf9cd43747610f8a2c8bf150e8ce0fb843
peskic93 <peshkic@gmail.com>
2016-11-06 19:59:41 +0100

    Change

--- app/src/main/java/solagro/org/solagro20/MainActivity.java
+++ app/src/main/java/solagro/org/solagro20/MainActivity.java
@@ -1,7 +1,9 @@
 package solagro.org.solagro20;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openMapActivity(View view){
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+
+    }
+
 }
---
+++ app/src/main/java/solagro/org/solagro20/MapActivity.java
@@ -0,0 +1,16 @@
+package solagro.org.solagro20;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+
+    }
+}
